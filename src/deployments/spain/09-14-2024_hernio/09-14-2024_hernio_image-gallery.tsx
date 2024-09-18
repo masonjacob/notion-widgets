2f13db9ac1eb438eaf17905ef0dc9b1fdb57097f
Mason Jacob <masonjacob@uri.edu>
2024-09-18 11:54:05 +0200

    update image sourcing

--- src/deployments/spain/09-14-2024_hernio/09-14-2024_hernio_image-gallery.tsx
+++ src/deployments/spain/09-14-2024_hernio/09-14-2024_hernio_image-gallery.tsx
@@ -3,7 +3,7 @@ import ImageGallery from "react-image-gallery";
 
 let images: {original: string, thumbnail: string}[] = [];
 
-let baseUrl = `${process.env.REACT_APP_HOSTNAME}/images/spain/09-14-2024_hernio`;
+let baseUrl = `${process.env.REACT_APP_HOSTNAME_PROD}/images/spain/09-14-2024_hernio`;
 
 for (let i = 1; i <= 50; i++) {
     images.push({
